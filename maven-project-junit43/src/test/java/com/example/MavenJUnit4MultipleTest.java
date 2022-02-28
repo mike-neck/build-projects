5db81736b210bd9287578a4f7b3fe4ceb9649634
mike_neck <jkrt3333@gmail.com>
2022-02-28 10:47:45 +0900

    Maven/JUnit4+3 のプロジェクトを追加

---
+++ maven-project-junit43/src/test/java/com/example/MavenJUnit4MultipleTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Theories.class)
+public class MavenJUnit4MultipleTest {
+
+     @DataPoints public static String[] data() {
+         return new String[] {
+                 "foo",
+                 "bar",
+                 "baz"
+         };
+     }
+
+     @Theory
+    public void test(String string) {
+         assertThat(string, is("FOO"));
+     }
+}
---
+++ maven-project-junit43/src/test/java/com/example/MavenJUnit4StructureTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Enclosed.class)
+public class MavenJUnit4StructureTest {
+
+    public static class First {
+        @Test
+        public void testFailure() {
+            assertThat("foo", is("bar"));
+        }
+    }
+
+    public static class Second {
+        @Test
+        public void testFailure() {
+            assertThat("bar", is("qux"));
+        }
+    }
+}
