ba731d295c87b25c43a5fd2082503c3c4e3857a9
mike-neck <jkrt3333@gmail.com>
2023-12-26 15:33:21 +0900

    add test pattern

--- gradle-project-junit5/src/test/java/com/example/NormalFailureTest.java
+++ gradle-project-junit5/src/test/java/com/example/NormalFailureTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,6 +44,35 @@ class NormalFailureTest {
     @ValueSource(ints = {1, 2, 3, 100, 200})
     void parameterTest(int value) {
         assertEquals(2 * value, value * value);
+    }
+
+    static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendOffsetId()
+            .toFormatter();
+
+    @RepeatedTest(10)
+    void repeatingTest() {
+        var now = Instant.now().atOffset(ZoneOffset.UTC);
+        String string = FORMATTER.format(now);
+        assertTrue(string.matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}Z"), string);
+    }
+
+    @RepeatedTest(10)
+    void repeatingInfo(RepetitionInfo info) {
+        int total = info.getTotalRepetitions();
+        int current = info.getCurrentRepetition();
+        int last = total - current;
+        int min = Math.min(last, current);
+        assertNotEquals(0, min % 3, () -> "total: %d current: %d last: %d, min: %d".formatted(
+                total, current, last, min
+        ));
     }
 
     @Disabled
